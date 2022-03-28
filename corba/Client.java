99a043f80699f0e23939ef1ae7f7196965045ab1
Vinh Truong <56877552+thvinhtruong@users.noreply.github.com>
2022-03-28 15:01:25 +0700

    fix stuffs

--- corba/Client.java
+++ corba/Client.java
@@ -3,8 +3,8 @@ public class Client {
         try { // initialize the ORB
             CORBA.ORB orb = CORBA.ORB.init();
             // bind to the Count server object, using the Count_var class generated from IDL
-            Counter.Count counter = Counter.Count_var.bind("My Exp"); // get ref. to remote obj.
-            counter.exp(2, 3); 
+            Exponential.Exp counter = Exponential.Exp.bind("My Exp"); // get ref. to remote obj.
+            System.out.println(counter.exp(2, 3)); 
         }
         catch (CORBA.SystemException e) {
             e.printStackTrace();

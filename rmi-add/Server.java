fc2cf1408d03df21482e34bd5afa9bc28c27355f
Vinh Truong <56877552+thvinhtruong@users.noreply.github.com>
2022-03-28 16:02:15 +0700

    fix another bug

--- rmi-add/Server.java
+++ rmi-add/Server.java
@@ -3,7 +3,7 @@ import java.rmi.*;
 class Server {
     public static void main (String[] argv) {
         try {
-            Naming.rebind("rmi://localhost/HelloServer", new Hello("Hello, world!"));
+            Naming.rebind("rmi://localhost/HelloServer", new AddTemp("Hello, world!"));
             System.out.println("Server is ready");
         }
         catch (Exception e) {
